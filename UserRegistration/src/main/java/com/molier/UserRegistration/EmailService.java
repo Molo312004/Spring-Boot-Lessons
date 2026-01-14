30a97e8234c9237422c0abcab9deb3bde85f23aa
Molo312004 <312004molo@gmail.com>
2026-01-15 00:38:32 +0530

    updated notification port and host

--- UserRegistration/src/main/java/com/molier/UserRegistration/EmailService.java
+++ UserRegistration/src/main/java/com/molier/UserRegistration/EmailService.java
@@ -2,15 +2,22 @@ package com.molier.UserRegistration;
 
 import com.molier.UserRegistration.Models.User;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service
 public class EmailService implements NotificationService{
+    @Value("${mail.host}")
+    String host;
+
+    @Value("${mail.port}")  
+    String port;
     public void sendNotification(String text, User user){
+        
         String name = user.getUserName();
         String email = user.getEmail();
 
         System.out.println(text);
         System.out.println("Recepient: " + name + "\n" + email);
-        System.out.println(user.getPassword()); //very bad practice to print password in real world applications lol
+        System.out.println("host: " + host + " port: " + port); 
     }
 }
